81a51750d5c754035e78d1423a7d41f2ee8aca88
Rony Man <ronyman.com@outlook.com>
2021-12-27 01:28:37 +1100

    2712_2

--- apps/user/views.py
+++ apps/user/views.py
@@ -44,20 +44,20 @@ def UserRegister(request):
 
         if len(username) > 15:
             messages.error(request, "Username must be under 15 characters.")
-            return redirect('forums/register')
+            return redirect('register')
         if not username.isalnum():
             messages.error(request, "Username must contain only letters and numbers.")
-            return redirect('forums/register')
+            return redirect('register')
         if password != confirm_password:
             messages.error(request, "Passwords do not match.")
-            return redirect('forums/register')
+            return redirect('register')
 
         user = User.objects.create_user(username, email, password)
         user.first_name = first_name
         user.last_name = last_name
         user.save()
-        return render(request, 'forums/login.html')
-    return render(request, "forums/register.html")
+        return render(request, 'login')
+    return render(request, "registration/register.html")
 
 def UserLogin(request):
     if request.method=="POST":
@@ -73,15 +73,15 @@ def UserLogin(request):
         else:
             messages.error(request, "Invalid Credentials")
         alert = True
-        return render(request, 'forums/login.html', {'alert':alert})
-    return render(request, "forums/login.html")
+        return render(request, 'login', {'alert':alert})
+    return render(request, "registration/login.html")
 
 def UserLogout(request):
     logout(request)
     messages.success(request, "Successfully logged out")
-    return redirect('forums/login')
+    return redirect('login')
 
-@login_required(login_url = 'forums/login')
+@login_required(login_url = 'registration/login.html')
 
 def myprofile(request):
     if request.method=="POST":
@@ -95,5 +95,8 @@ def myprofile(request):
             return render(request, "profile/profile.html",{'obj':obj})
     else:
         form=ProfileForm()
-    return render(request, "profile/profile.html", {'form':form})
+    return render(request, 'profile', {'form':form})
 
+def password_reset(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "profile/password_reset.html")
