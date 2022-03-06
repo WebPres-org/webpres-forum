15ff02f85feac1ff6d2b8057c2220365f6572de5
Rony Man <ronyman.com@outlook.com>
2022-03-06 14:20:34 +1100

    Update requirement text

--- Forum/settings.py
+++ Forum/settings.py
@@ -1,11 +1,11 @@
 
-from pathlib import Path
 import os
 import django_heroku
 import sys
-from django.core.mail import EmailMultiAlternatives
-
+from django.conf import settings
 sys.modules['fontawesome_free'] = __import__('fontawesome-free')
+
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'filebrowser',
     'fontawesome_free',
     'storages',
+    'graphene_django',
 
 ]
 
@@ -145,7 +146,28 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {'django.request': {'handlers': ['mail_admins'], 'level': 'ERROR', 'propagate': True}},
+}
 
 # Settings for django-bootstrap-v5
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Forum/urls.py
+++ Forum/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from apps.forum_core import views as user_views
 from django.shortcuts import render
+from graphene_django.views import GraphQLView
 
 #from .forms import UserRegisterForm, UserUpdateForm, EditProfileForm, ProfileForm
 #from .models import Profile, UpdateProfileForm
@@ -17,4 +18,5 @@ urlpatterns = [
     # Forum_core
     path('admin/', admin.site.urls),
     path('', include('apps.forum_core.urls')),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
